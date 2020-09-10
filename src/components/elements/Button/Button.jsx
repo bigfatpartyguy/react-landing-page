7cbca68eade91c3f1d6df91644ec3662d2a30424
Maksim Morozov <maxmorozov.art@gmail.com>
2020-09-10 16:28:03 +0300

    add styles for nav sections

--- src/components/elements/Button/Button.jsx
+++ src/components/elements/Button/Button.jsx
@@ -1,10 +1,18 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
-import Image from '../Image';
 import styles from './Button.module.scss';
 
 // eslint-disable-next-line react/prop-types
 const Button = ({className, disabled, icon, type, onClick, children}) => {
+  let iconContainer;
+  if (icon || Array.isArray(children)) {
+    iconContainer = (
+      <span className={styles['btn-icon__icon-container']}>
+        {icon || children[0]}
+      </span>
+    );
+  }
   return (
     // eslint-disable-next-line react/button-has-type
     <button
@@ -12,8 +20,17 @@ const Button = ({className, disabled, icon, type, onClick, children}) => {
       disabled={disabled}
       type={type}
       onClick={onClick}>
-      {icon || ''}
-      {children}
+      {Array.isArray(children) ? (
+        <>
+          {iconContainer}
+          {children.slice(1)}
+        </>
+      ) : (
+        <>
+          {iconContainer || ''}
+          {children}
+        </>
+      )}
     </button>
   );
 };
